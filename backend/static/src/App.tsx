6282dbe8c706d2df8917de005e097529aef2f308
Matt Linder <linder2015@outlook.com>
2024-09-19 12:50:05 -0400

    using prod api url

--- backend/static/src/App.tsx
+++ backend/static/src/App.tsx
@@ -1,7 +1,7 @@
 import HomePage from "./pages/home/Home";
 
-export const SERVER = "http://127.0.0.1:5000"; // dev
-// export const SERVER = ""; // prod
+// export const SERVER = "http://127.0.0.1:5000"; // dev
+export const SERVER = "https://capstoneproject-5z6p.onrender.com"; // prod
 
 export default function App() {
   return <HomePage />;
